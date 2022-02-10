81065a51f047daeee3660217f3c751ce6065953c
ibeandyy <adhyr4@gmail.com>
2022-02-09 19:48:11 -0800

    Resolve bugfix for multiple tracks

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from settings import *
 
 client = discord.Client()
 
-
 @client.event
 async def on_ready():
     print("\nBot is operational as {0.user}".format(client))
-
 
 @client.event
 async def on_message(message):
@@ -18,17 +16,30 @@ async def on_message(message):
     for embed in embeds:
         embed_message += str(embed.to_dict())
 
-    if words_to_scan in embed_message and message.author != client.user:
+    if words_to_scan in embed_message and message.author!=client.user:
         print(embed_message)
-        embed_message = embed_message.split("'description': '", 1)[1]
-        embed_message = embed_message.split("'title'", 1)[0]
+        embed_message = embed_message.split("'description': ",1)[1]
+        embed_message = embed_message.split("'title'",1)[0]
         channel = client.get_channel(NowPlayingChannelID)
-        embed = discord.Embed(title=words_to_scan,
-                              description=embed_message[:-3],
-                              color=0x1a9cb6)
-        embed.set_thumbnail(url=img_url)
-        embed.set_footer(text="Concave Co-op - The world's best kept secret.", icon_url=img_url)
-        await channel.send(embed=embed)
 
+        if message.author.id == HypeBotID:
+            embed = discord.Embed(title = words_to_scan,
+                                description = embed_message[:-3],
+                                color = 0xd96c00)
+            embed.set_author(name="Rave-Stadium")
+            embed.set_thumbnail(url=img_url)
+            embed.set_footer(text = "Concave Co-op - The world's best kept secret.", icon_url = img_url)
+
+        elif message.author.id == VibesBotID:
+            embed = discord.Embed(title = words_to_scan,
+                                description = embed_message[:-3],
+                                color = 0x04d900)
+            embed.set_author(name="Cave-Stadium")
+            embed.set_thumbnail(url=img_url)
+            embed.set_footer(text = "Concave Co-op - The world's best kept secret.", icon_url = img_url)
+            print("Embed two : ", embed)
+            
+        await channel.send(embed=embed)
+        
 
 client.run(discordBotToken)
--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
+import os
+
 words_to_scan = "Now playing"
-discordBotToken = ""
+discordBotToken = os.environ["bottoken"]
 img_url = "https://i.postimg.cc/GmDwBbD4/CNV-white.png"
-NowPlayingChannelID = 927784768681156618
+NowPlayingChannelID = 886310094441156631
+HypeBotID = 123456
+VibesBotID = 654321
